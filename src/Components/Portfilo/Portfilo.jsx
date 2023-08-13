798c808930918e87ad7ba1058cdd5de3501049df
779475324 <101010111100b@gmail.com>
2023-08-14 02:03:43 +0300

    fixed

--- src/Components/Portfilo/Portfilo.jsx
+++ src/Components/Portfilo/Portfilo.jsx
@@ -3,12 +3,12 @@ import css from './Portfilo.module.scss'
 import { motion } from 'framer-motion'
 import { fadeIn, staggerChildren } from '../../utils/motion'
 import { BiStreetView } from 'react-icons/bi'
-import Delivery from '../../../public/images/Delivery.jpg';
-import gym from '../../../public/images/gym.jpg';
-import AcademiLearning from '../../../public/images/website AcademiLearning.jpg';
-import timerTime from '../../../public/images/timerTime.jpg';
-import NonaProduct from '../../../public/images/NanoCamp.jpg';
-import Watch from '../../../public/images/Smart Watch.jpg';
+// import Delivery from '../../../public/images/Delivery.jpg';
+// import gym from '../../../public/images/gym.jpg'; 
+// import AcademiLearning from '../../../public/images/website AcademiLearning.jpg'; 
+// import timerTime from '../../../public/images/timerTime.jpg'; 
+// import NonaProduct from '../../../public/images/NanoCamp.jpg';
+// import Watch from '../../../public/images/Smart Watch.jpg'; 
 export const Portfilo = () => {
   return (
     <motion.section
@@ -37,7 +37,7 @@ export const Portfilo = () => {
 
            <motion.div className={ css.card}  variants={fadeIn("down","tween",0.5,0.6)} >
     <div  className={ css.cardimage} >
-      <img   src={Delivery}alt="Project" /> 
+      <img  src="./images/Delivery.jpg"  alt="Project" /> 
     </div>
     <div className={ css.category}> <a  href="">Github</a>  </div>
     <div className={ css.heading} > project Delivery HTML+CSS+js+JQuery+Php
@@ -46,7 +46,7 @@ export const Portfilo = () => {
 </motion.div>
 <div className={ css.card}>
     <div  className={ css.cardimage} >
-      <img    src={gym} alt="Project"  style={{ height:"270px" }}/>
+      <img  src="./images/gym.jpg"  alt="Project"  style={{ height:"270px" }}/>
     </div>
     <div className={ css.category}> <a  href="">Github</a>  </div>
     <div className={ css.heading} > project Delivery <br/> using : HTML+CSS+js+JQuery+Php
@@ -56,7 +56,7 @@ export const Portfilo = () => {
 
 <motion.div className={ css.card}  variants={fadeIn("right","tween",0.5,0.6)} >
     <div  className={ css.cardimage} >
-      <img  src={AcademiLearning} alt="Project"  style={{ height:"270px" }} />
+      <img  src="./images/website AcademiLearning.jpg" alt="Project"  style={{ height:"270px" }} />
     </div>
     <div className={ css.category}> <a  href="">Github</a>  </div>
     <div className={ css.heading} > Education Online <br/> using :HTML+CSS
@@ -66,7 +66,7 @@ export const Portfilo = () => {
 
 <motion.div className={ css.card}  variants={fadeIn("up","tween",0.5,0.6)} >
     <div  className={ css.cardimage} >
-      <img   src={Watch} alt="Project"  style={{ height:"270px" }}  />
+      <img src="./images/Smart Watch.jpg" alt="Project"  style={{ height:"270px" }}  />
     </div>
     <div className={ css.category}> <a  href="">Github</a>  </div>
     <div className={ css.heading} > Store Smart Watch<br/> using : HTML+CSS
@@ -78,7 +78,7 @@ export const Portfilo = () => {
 
 <div className={ css.card}>
     <div  className={ css.cardimage} >
-      <img     src={timerTime} alt="Project" /> 
+      <img src="./images/timerTime.jpg" alt="Project" /> 
     </div>
     <div className={ css.category}> <a  href="">Github</a>  </div>
     <div className={ css.heading} > Timer Time <br/>  using : HTML+CSS+js+API
@@ -89,7 +89,7 @@ export const Portfilo = () => {
 
 <motion.div className={ css.card}  variants={fadeIn("up","tween",0.5,0.6)} >
     <div  className={ css.cardimage} >
-      <img    src={NonaProduct} alt="Project"    />
+      <img src="./images/NanoCamp.jpg" alt="Project"   style={{ height:"270px" }}  />
     </div>
     <div className={ css.category}> <a  href="">Github</a>  </div>
     <div className={ css.heading} > Store webSite <br/>  using : HTML+CSS+js
--- src/Components/Skilles/Skill.jsx
+++ src/Components/Skilles/Skill.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import css from "./Skill.module.scss";
-import boot from '../../../public/iconpng/Bootstrap.png';
-import circleHtml from '../../../public/iconpng/circleHtml.png';
+// import boot from '../../../public/iconpng/Bootstrap.png';
+import circleHtml from '../../../public/iconpng/circleHtml.png'; 
 import circlePhp from '../../../public/iconpng/circlePhp.png.png';
-import Css from '../../../public/iconpng/Css.png';
-import Js from '../../../public/iconpng/Js.png';
-import Mysql from '../../../public/iconpng/Mysql.png';
-import Xd from '../../../public/iconpng/Xd.png';
-import Sass from '../../../public/iconpng/Sass.png';
+import Css from '../../../public/iconpng/Css.png'; 
+import Js from '../../../public/iconpng/Js.png';  
+import Mysql from '../../../public/iconpng/Mysql.png'; 
+import Xd from '../../../public/iconpng/Xd.png'; 
+import Sass from '../../../public/iconpng/Sass.png'; 
 import Phot from '../../../public/iconpng/Photoshop.png';
 // import github from '../../../public/iconpng/github.png';
-import Api from '../../../public/iconpng/Api.png';
+import Api from '../../../public/iconpng/Api.png'; 
 import Oracle from '../../../public/iconpng/Oracle.png';
 import react from '../../../public/iconpng//1611079.png';
 import tailwind  from'../../../public/iconpng/ta.png';
@@ -23,35 +23,35 @@ export const Skill = () => {
     
         <div  className={`flexCenter ypaddings innerWidth ${ css.StyleBox }`}>
         <div className={css.box}>
-            <img src={circleHtml} alt="php" />
+            <img src="./iconpng/circleHtml.png" alt="php" />
             <p >HTML</p>
         </div>
         <div className={css.box}>
-            <img src={circlePhp} alt="php" />
+            <img  src="./iconpng/circlePhp.png.png" alt="php" />
             <p>PHP</p>
         </div>
         <div className={css.box}>
-            <img src={Js} alt="js" />
+            <img src="./iconpng/Js.png" alt="js" />
             <p>JavaScript</p>
         </div>
         <div className={css.box}>
-        <img src={Css} alt="reactJS" />
+        <img  src="./iconpng/Css.png" alt="reactJS" />
             <p>Css</p>
         </div>
         <div className={css.box}>
-        <img src={react} alt="reactJS" />
+        <img src="./iconpng/1611079.png" alt="reactJS" />
             <p>ReactJs</p>
         </div>
         <div className={css.box}>
-        <img src={Sass} alt="sass" />
+        <img src="./iconpng/Sass.png" alt="sass" />
             <p>SASS</p>
         </div>
         <div className={css.box}>
-        <img src={boot} alt="Bootstrap" />
+        <img src="./iconpng/Bootstrap.png" alt="Bootstrap" />
             <p>Bootstrap</p>
         </div>
         <div className={css.box}>
-        <img src={tailwind} alt="photoshop" />
+        <img src="./iconpng/ta.png" alt="photoshop" />
             <p>Tailwind CSS</p>
         </div>
         {/* <div className={css.box}>
@@ -59,23 +59,23 @@ export const Skill = () => {
             <p>Mlogger</p>
         </div> */}
         <div className={`${css.box}`} >
-        <img src={Xd} alt="Xd" />
+        <img src="./iconpng/Xd.png" alt="Xd" />
             <p>Adobe XD</p>
         </div>
         <div className={css.box}>
-        <img src={Phot} alt="Photoshop" />
+        <img  src="./iconpng/Photoshop.png" alt="Photoshop" />
             <p>Photoshop</p>
         </div>
         <div className={css.box}>
-            <img src={Oracle} alt="Oracle" />
+            <img  src="./iconpng/Oracle.png" alt="Oracle" />
             <p>Oracle</p>
         </div>
         <div className={css.box}>
-            <img src={Mysql} alt="php" />
+            <img src="./iconpng/Mysql.png" alt="php" />
             <p>MySql</p>
         </div>
         <div className={css.box}>
-            <img src={Api} alt="Api" />
+            <img  src="./iconpng/Api.png" alt="Api" />
             <p>API's</p>
         </div>
      
