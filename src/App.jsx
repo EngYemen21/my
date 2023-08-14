768eaa362222ad3d5ddfbc83c24a07e50c7b57e8
779475324 <101010111100b@gmail.com>
2023-08-14 03:19:56 +0300

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import { Header } from "./Components/Header/Header";
 import { Hero } from "./Components/Hero/Hero";
 import { Portfilo } from "./Components/Portfilo/Portfilo";
 import { Skill } from "./Components/Skilles/Skill";
-
+import global from './styles/global.scss';
 import css from "./styles/app.module.scss";
 const App = () => {
   return (
-    <div className={`bg-primary background`} style={{ color: "white" }}>
+    <div className={`bg-primary background`} style={{ color: "white", background:url('./wallpaper6.jpg') }}>
       <Header />
       <Hero />
       <Experties />
